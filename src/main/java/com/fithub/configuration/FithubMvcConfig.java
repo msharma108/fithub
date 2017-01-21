511d18afa5acf8d6c14b5c1b557b484256a12613
Mohit <sharmamohits.ms@gmail.com>
2017-01-21 18:04:31 -0500

    basic cloud configuration done. need to configure database for cloud next. Automatic deployment enabled on master branch.

--- src/main/java/com/fithub/configuration/FithubMvcConfig.java
+++ src/main/java/com/fithub/configuration/FithubMvcConfig.java
@@ -1,9 +1,7 @@
 package com.fithub.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -14,15 +12,12 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 public class FithubMvcConfig extends WebMvcConfigurerAdapter {
 
-	@Autowired
-	Environment env;
-
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
-		System.out.println(env.getProperty("server.port"));
+
 		return resolver;
 	}
 }
--- src/main/java/com/fithub/configuration/TomcatMultiConnectorConfig.java
+++ src/main/java/com/fithub/configuration/TomcatMultiConnectorConfig.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Profile;
  *
  */
 @Configuration
-@Profile("!heroku")
+@Profile("!cloud")
 public class TomcatMultiConnectorConfig {
 
 	// Reference:-
