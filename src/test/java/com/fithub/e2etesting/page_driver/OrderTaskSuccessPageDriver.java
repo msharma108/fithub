63bc575f785be819077ae797ae2639d9ed3c2d06
Mohit <sharmamohits.ms@gmail.com>
2017-01-21 12:43:09 -0500

    noticed issues with the previous wait, added another form of explicit wait for modal element

--- src/test/java/com/fithub/e2etesting/page_driver/OrderTaskSuccessPageDriver.java
+++ src/test/java/com/fithub/e2etesting/page_driver/OrderTaskSuccessPageDriver.java
@@ -1,12 +1,12 @@
 package com.fithub.e2etesting.page_driver;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class OrderTaskSuccessPageDriver extends HeaderPageDriver {
@@ -25,16 +25,12 @@ public class OrderTaskSuccessPageDriver extends HeaderPageDriver {
 
 	public boolean handleOrderTaskSuccess() {
 
-		// wait half a second for the success modal to show up on HTML
-		// try {
-		// Thread.sleep(900);
-		// } catch (InterruptedException e) {
-		// e.printStackTrace();
-		// }
+		// added explicit wait for booking success modal
+		WebElement orderBookingPopupWebElement = (new WebDriverWait(driver, 2))
+				.until(ExpectedConditions.visibilityOf(orderBookingSuccessMessagePopup));
 
-		driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-		boolean orderSuccess = this.orderBookingSuccessMessagePopup.isDisplayed();
-		this.orderBookingSuccessMessagePopup.click();
+		boolean orderSuccess = orderBookingPopupWebElement.isDisplayed();
+		orderBookingPopupWebElement.click();
 		return orderSuccess;
 
 	}
