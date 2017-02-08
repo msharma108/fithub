9138e27080b1446c2d80611fb210b6121645aac5
MOHIT\Mohit <sharmamohits.ms@gmail.com>
2017-02-08 01:10:36 -0500

    refactored few unit tests

--- src/test/java/com/fithub/unittests/ShoppingCartServiceTest.java
+++ src/test/java/com/fithub/unittests/ShoppingCartServiceTest.java
@@ -1,5 +1,7 @@
 package com.fithub.unittests;
 
+import static org.junit.Assert.assertEquals;
+
 import java.math.BigDecimal;
 
 import org.junit.Before;
@@ -25,22 +27,27 @@ public class ShoppingCartServiceTest {
 	}
 
 	@Test
-	public void updateShoppingCartAddsProductToCartIfProductInStockAndNotInCart() {
+	public void updateShoppingCartAddsProductToCartAndUpdatesCartCostIfProductInStockAndNotInCart() {
 		ShoppingCartService shoppingCartService = new ShoppingCartServiceImpl();
 		int stockQuantity = 2;
-		ProductDTO productDTO = prepareProductDTOForTest(stockQuantity);
+		BigDecimal productPrice = BigDecimal.TEN;
+		BigDecimal expectedCartCost = (productPrice.multiply(shoppingCart.getCartTaxRate())).add(productPrice);
+		ProductDTO productDTO = prepareProductDTOForTest(stockQuantity, productPrice);
 		String shoppingCartOperationType = "addToCart";
 
 		shoppingCartService.updateShoppingCart(shoppingCart, productDTO, shoppingCartOperationType, BigDecimal.ZERO,
 				BigDecimal.ZERO);
-
+		// Assert shopping cart has 1 item
+		assertEquals("Shopping cart empty, product not added", 1, shoppingCart.getCartProductList().size());
+		assertEquals("Shopping cart cost not updated", expectedCartCost, shoppingCart.getCartTotalCost());
 	}
 
 	@Test
 	public void updateShoppingCartDoesNotAddProductToCartIfProductNotInStock() {
 		ShoppingCartService shoppingCartService = new ShoppingCartServiceImpl();
 		int stockQuantity = 0;
-		ProductDTO productDTO = prepareProductDTOForTest(stockQuantity);
+		BigDecimal productPrice = BigDecimal.TEN;
+		ProductDTO productDTO = prepareProductDTOForTest(stockQuantity, productPrice);
 		String shoppingCartOperationType = "addToCart";
 
 		expectedException.expect(IllegalStateException.class);
@@ -53,11 +60,33 @@ public class ShoppingCartServiceTest {
 
 	}
 
-	public ProductDTO prepareProductDTOForTest(int stockQuantity) {
+	@Test
+	public void updateShoppingCartRemovesProductFromCartAndUpdatesCartCostIfProductInCart() {
+		ShoppingCartService shoppingCartService = new ShoppingCartServiceImpl();
+		int stockQuantity = 2;
+		String shoppingCartOperationType = "removeFromCart";
+		BigDecimal productPrice = BigDecimal.TEN;
+		ProductDTO productDTO = prepareProductDTOForTest(stockQuantity, productPrice);
+		// setup test data by adding product to cart
+		shoppingCart.getCartProductList().add(productDTO);
+		shoppingCart.setCartCost(productPrice);
+		shoppingCart.setCartTax(shoppingCart.getCartCost().multiply(shoppingCart.getCartTaxRate()));
+		shoppingCart.setCartTotalCost(shoppingCart.getCartTax().add(shoppingCart.getCartCost()));
+
+		shoppingCartService.updateShoppingCart(shoppingCart, productDTO, shoppingCartOperationType,
+				productDTO.getPrice(), BigDecimal.ONE);
+
+		// Assert shopping cart has 1 item
+		assertEquals("Product not removed from shopping cart", 0, shoppingCart.getCartProductList().size());
+		assertEquals("Shopping cart cost not updated", BigDecimal.ZERO.setScale(2, 0), shoppingCart.getCartTotalCost());
+
+	}
+
+	public ProductDTO prepareProductDTOForTest(int stockQuantity, BigDecimal productPrice) {
 		ProductDTO productDTO = new ProductDTO();
 		productDTO.setStockQuantity(stockQuantity);
 		productDTO.setQuantityInCart(BigDecimal.ZERO);
-		productDTO.setPrice(BigDecimal.TEN);
+		productDTO.setPrice(productPrice);
 		productDTO.setProductName("productBeingAddedFirstTimeToCart");
 		return productDTO;
 	}
