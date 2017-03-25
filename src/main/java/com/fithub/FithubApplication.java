560665cba6200271c597942dacedbf841b9d261f
MOHIT\Mohit <sharmamohits.ms@gmail.com>
2017-03-25 16:14:41 -0400

    Added code to implement war deployment on non-embedded web/app servers

--- src/main/java/com/fithub/FithubApplication.java
+++ src/main/java/com/fithub/FithubApplication.java
@@ -6,11 +6,19 @@ package com.fithub;
  */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FithubApplication {
+public class FithubApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FithubApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FithubApplication.class);
+	}
+
 }
